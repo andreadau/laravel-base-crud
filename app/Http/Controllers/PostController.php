877ff08ef0549ea04587b27d15e10af740ce6cb9
Andrea <dauriaandrea99@gmail.com>
2021-02-19 22:52:55 +0100

    Post Order fix

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,7 +14,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::all();
+        $posts = Post::latest()->get();
         //dd($posts);
         return view('posts.index', compact('posts'));
     }
