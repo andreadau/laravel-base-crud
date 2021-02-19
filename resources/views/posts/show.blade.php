35d69ea0d63b981e8b378f58d6cd4d8649b2d490
Andrea <dauriaandrea99@gmail.com>
2021-02-19 17:37:35 +0100

    Post Show Add

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,9 +1,8 @@
-@extends('layout.app')
+@extends('layout.app');
 @section('main')
     <main>
-        <div class="container">
-        <a href="{{ route('posts.create') }}">NEW POST</a>
-            @foreach($posts as $post)
+    <div class="container">
+        <a href="{{ route('posts.index') }}">BACK TO POST</a>
                 <div class="posts">
                     <h2>{{$post->title}}</h2>
                     <p>{{$post->description}}</p>
@@ -11,7 +10,6 @@
                     <a href="#"><span>EDIT</span></a> | 
                     <a href="#"><span>DELETE</span></a><p>
                 </div>
-            @endforeach
         </div>
     </main>
 @endsection
